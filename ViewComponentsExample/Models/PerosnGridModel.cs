f0375e12a8645dacd5b6b2e26a684351466f952c
Hassan Koraim <hassankoraim2@gmail.com>
2024-05-13 20:53:45 +0200

    112. View Components with ViewData Lesson

---
+++ ViewComponentsExample/Models/PerosnGridModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewComponentsExample.Models
+{
+    public class PerosnGridModel
+    {
+        public string? GridTitle { get; set; } = string.Empty;
+        public List<Person> persons { get; set; } = new List<Person>();
+    }
+}
--- ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.ViewComponents
 {
@@ -6,6 +7,21 @@ namespace ViewComponentsExample.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            PerosnGridModel model = new PerosnGridModel()
+            {
+                GridTitle = "Perosn List",
+                persons = new List<Person>() {
+                    new Person {PersonName = "Hassan", JobTitle = "Manager"},
+                    new Person {PersonName = "Hussien", JobTitle = "Senior"},
+                    new Person {PersonName = "Mohamed", JobTitle = "Junior"},
+                    new Person {PersonName = "Ahmed", JobTitle = "DAs"},
+                    new Person {PersonName = "Abdallah", JobTitle = "Doctor"},
+
+                }
+
+            };
+            ViewData["Grid"] = model;
+
             return View("Sample"); // Invoked the partial view in Views/Shared/Components/Grid/Default.cshtml
         }
     }
